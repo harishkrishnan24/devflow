8c39e0ff873563eb302a062e5955562c0702c304
Harish Krishnan <harishkrishnan1993@gmail.com>
2024-07-13 15:42:24 +0530

    get auth user from clerk

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,10 +1,10 @@
 import Question from "@/components/forms/Question";
 import { getUserById } from "@/lib/actions/user.action";
+import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 const AskQuestion = async () => {
-  // const { userId } = auth();
-  const userId = "12345";
+  const { userId } = auth();
 
   if (!userId) {
     redirect("/sign-in");
