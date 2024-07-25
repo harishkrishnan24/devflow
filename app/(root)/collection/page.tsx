450bf2cccdecb40fb6ebe6c6d232a099d5d1eb86
Harish Krishnan <harishkrishnan1993@gmail.com>
2024-07-25 13:26:29 +0530

    feat: collections page search

--- app/(root)/collection/page.tsx
+++ app/(root)/collection/page.tsx
@@ -4,14 +4,16 @@ import NoResult from "@/components/shared/NoResult";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { QuestionFilters } from "@/constants/filters";
 import { getSavedQuestions } from "@/lib/actions/user.action";
+import { SearchParamsProps } from "@/types";
 import { auth } from "@clerk/nextjs/server";
 
-const Collection = async () => {
+const Collection = async ({ searchParams }: SearchParamsProps) => {
   const { userId } = auth();
   if (!userId) return null;
 
   const result = await getSavedQuestions({
     clerkId: userId,
+    searchQuery: searchParams.q,
   });
 
   return (
