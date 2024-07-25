bebe052c3deeb4c3207ef7a0ebebe423c4949a30
Harish Krishnan <harishkrishnan1993@gmail.com>
2024-07-25 13:23:54 +0530

    feat: community page search

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -81,8 +81,20 @@ export async function deleteUser(params: DeleteUserParams) {
 export async function getAllUsers(params: GetAllUsersParams) {
   try {
     await connectToDatabase();
-    // const { filter, page = 1, pageSize = 20, searchQuery } = params;
-    const users = await User.find({}).sort({ createdAt: -1 });
+    const { searchQuery } = params;
+    const query: FilterQuery<typeof User> = {};
+
+    if (searchQuery) {
+      query.$or = [
+        {
+          name: { $regex: new RegExp(searchQuery, "i") },
+        },
+        {
+          username: { $regex: new RegExp(searchQuery, "i") },
+        },
+      ];
+    }
+    const users = await User.find(query).sort({ createdAt: -1 });
     return { users };
   } catch (error) {
     console.error(error);
